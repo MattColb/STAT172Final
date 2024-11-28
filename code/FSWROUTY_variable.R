b2b57c99cbb050ba28ff54a316c55563d1f991df
PhuongNg2003 <nguyenphuong.business03@gmail.com>
2024-11-28 17:41:53 -0600

    adding clusters and do some classifications

--- code/FSWROUTY_variable.R
+++ code/FSWROUTY_variable.R
@@ -60,29 +60,69 @@ ggplot(data = data_X_long) +
   scale_fill_brewer("Cluster \nMembership", palette = "Dark2") +
   ggtitle("Hierachical Clusters")
 
+# Cluster 1: small households, elderly or retired individuals living alone or with a spouse
+#           => might benefit us to look more into this cluster
+# Cluster 2: young black families
+# Cluster 3: large families with children 
+# Cluster 4: medium size Hispanic families
+#           => big family size with children and elderly  
+# Cluster 5: Outliers or Minimal Engagement
 
 ##########################################
 ############ Train Test Split ############
 #########################################
 
+# Add cluster membership back to the main dataset
+cps_data$h_cluster <- as.factor(cutree(data_clust, k = 5))
+
+# Combine cluster memberships with other features
+model_data <- cps_data[, c("FSWROUTY_bin", "h_cluster", "weight", "hhsize", "female", 
+                           "hispanic", "black", "kids", "elderly", "education", "married")]
+
 # splitting training and testing
 RNGkind(sample.kind = "default")
 set.seed(159159)
-train.idx <- sample(x=1:nrow(cps_data), size=.7*nrow(cps_data))
-train.df <- cps_data[train.idx,]
-test.df <- cps_data[-train.idx,]
+train.idx <- sample(x=1:nrow(model_data), size=.7*nrow(model_data))
+train.df <- model_data[train.idx,]
+test.df <- model_data[-train.idx,]
+
+##############################################
+################ MLE #########################
+##############################################
+
+fswrouty_mle <- glm(FSWROUTY_bin ~ h_cluster + weight +hhsize + married + 
+                      education + elderly + kids + black + hispanic + female,
+                    data = train.df,
+                    family = binomial(link = 'logit'))
+
+summary(fswrouty_mle)
+# weight has a strong positive effect: Higher weights increase the likelihood of "Yes."
+# education and elderly have negative effects
+# black and hispanic have positive effects
+
+# Evaluate Model Accuracy 
+test_pred_probs <- predict(fswrouty_mle, newdata = test.df, type = "response")
+test_pred_classes <- ifelse(test_pred_probs > 0.5, "Yes", "No")
+library(caret)
+confusionMatrix(as.factor(test_pred_classes), test.df$FSWROUTY_bin)
+
+# the model correctly classifies 67.45% of all instances.
+# sensitivity = 46/(232 + 46) = 16.55%
+# specificity = 497/(497 + 30) = 94.31%
 
 ###############################################
 ############# RANDOM FOREST ###################
 ##############################################
 
-rf_fswrouty <- randomForest(FSWROUTY_bin ~ hhsize + married + education + elderly +
-                                      kids + black + hispanic + female ,
+rf_fswrouty <- randomForest(FSWROUTY_bin ~ h_cluster + weight +hhsize + married + 
+                              education + elderly + kids + black + hispanic + female ,
                            data = train.df,
                            ntree = 1000,
                            mtry = 4,
                            importance = TRUE)
 
+importance(rf_fswrouty)
+varImpPlot(rf_fswrouty)
 # Validate model as predictive tool
 
 pi_hat <- predict(rf_fswrouty, test.df, type = "prob")[, "Yes"] #Choose positive event column
@@ -93,19 +133,12 @@ rocCurve <- roc(response = test.df$FSWROUTY_bin,
 
 plot(rocCurve, print.thres = TRUE, print.ouc = TRUE)
 
-auc(rocCurve) # Area under the curve: 0.6338
+# 72.3% of positive cases are correctly classified
+# 49.7% of negative cases are misclassified as positive
 
-##############################################
-################ MLE #########################
-##############################################
+auc(rocCurve) # Area under the curve: 0.6225
 
-fswrouty_mle <- glm(FSWROUTY_bin ~ hhsize + married + education + elderly +
-                      kids + black + hispanic + female,
-                    data = train.df,
-                    family = binomial(link = 'logit'), 
-                    weights = weight)
 
-beta <- fswrouty_mle %>% coef()
 
 
 
