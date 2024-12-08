ef74e38d361b36563f5fb2baea50c3f96552e031
Matt Colbert <colbertmatt12@gmail.com>
2024-12-08 15:12:52 -0600

    Finding average rank

--- code/combining_predictions.R
+++ code/combining_predictions.R
@@ -8,10 +8,16 @@ fswrouty <- read.csv("./data/fswrouty_prediction.csv")
 
 fsstmp <- read.csv("./data/fsstmp_prediction.csv")
 
+fsfoods <- read.csv("./data/fsfoods_prediction.csv")
+
 head(fsstmp)
 
 merged <- fsstmp %>%  left_join(fswrouty, by="serialno") %>% 
   select("serialno", "PUMA.x", "fswrouty_probs", "fsstmp_probabilities", "hhsize.x", "weight.x", "elderly.x")
+
+merged <- merged %>%  left_join(fsfoods, by="serialno") %>% 
+  select("serialno", "PUMA.x", "fswrouty_probs", "fsstmp_probabilities", "fsfoods_prop_preds",
+         "hhsize.x", "weight.x", "elderly.x")
 
 names(merged)[names(merged) == 'PUMA.x'] <- 'PUMA'
 names(merged)[names(merged) == 'hhsize.x'] <- 'hhsize'
@@ -20,23 +26,15 @@ names(merged)[names(merged) == 'weight.x'] <- 'weight'
 
 names(merged)[names(merged) == 'elderly.x'] <- 'elderly'
 
-merged_standard <- merged
-
-merged_standard[,c("fswrouty_probs", "fsstmp_probabilities")] <- apply(merged, 2, function(x)(x - mean(x))/sd(x))[,c("fswrouty_probs", "fsstmp_probabilities")]
-
-merged_standard <- merged_standard %>% mutate(
-  mean_of_z_scores = (fswrouty_probs + fsstmp_probabilities)/2
-)
-
-ggplot(data=merged_standard) + geom_histogram(aes(x=sum_of_z_scores))
-
-summary_by_PUMA <- merged_standard %>% group_by(PUMA = as.factor(PUMA)) %>% 
+summary_by_PUMA <- merged %>% group_by(PUMA = as.factor(PUMA)) %>% 
   summarise(
     sample_size = sum(hhsize),
-    food_insecurity_metric = weighted.mean(fsstmp_probabilities, weight),
-    proportion_of_single_senior_households = mean(elderly == hhsize & hhsize==1),
-    single_senior = sum(elderly == hhsize & hhsize==1)
-  ) %>% as.data.frame() %>% arrange(desc(food_insecurity_metric))
+    fsstmp = weighted.mean(fsstmp_probabilities, weight),
+    fsfoods = weighted.mean(fsfoods_prop_preds, weight),
+    fswrouty = weighted.mean(fswrouty_probs, weight),
+  ) %>% as.data.frame()
+
+
 
 sf_data <- st_read("./data/tl_2023_19_puma20/tl_2023_19_puma20.shp")
 
@@ -44,15 +42,6 @@ colnames(sf_data)[colnames(sf_data) == "GEOID20"] = "PUMA"
 
 map_data <- sf_data %>%
   left_join(summary_by_PUMA, by = "PUMA")
-
-#Proportion of seniors that are on SNAP/Food Stamps
-ggplot(data = map_data) +
-  geom_sf(aes(fill = food_insecurity_metric)) +
-  scale_fill_viridis_c(option = "plasma") +  # Adjust color palette as needed
-  theme_minimal() +
-  labs(title = "Proportion of Households with Seniors on SNAP/Food Stamps",
-       fill = "Proportion on\nFood Stamps/SNAP")
-ggsave("figures/test.png", width=6, height=5)
 
 
 senior_data <- read.csv("./data/iowa_seniors_by_puma.csv")
@@ -62,13 +51,27 @@ senior_data <- senior_data %>% mutate("PUMA" = as.character(GEOID))
 senior_data <- map_data %>% left_join(senior_data, by="PUMA")
 
 senior_data <- senior_data %>% mutate(
-  seniors_on_fsstmp = floor(food_insecurity_metric*senior_population)
-) 
+  fsstmp_num_seniors = fsstmp * senior_population, 
+  fsfoods_num_seniors = fsfoods * senior_population, 
+  fswrouty_num_seniors = fswrouty * senior_population 
+)
+
+senior_data <- senior_data %>% 
+  mutate(
+    rank_fsstmp = rank(-fsstmp_num_seniors),
+    rank_fsfoods = rank(-fsfoods_num_seniors),
+    rank_fswrouty = rank(-fswrouty_num_seniors)
+  )
+
+senior_data <- senior_data %>% 
+  mutate(
+    average_rank = (rank_fsstmp + rank_fsfoods + rank_fswrouty)/3
+  )
 
 ggplot(data = senior_data) +
-  geom_sf(aes(fill = seniors_on_fsstmp)) +
-  scale_fill_viridis_c(option = "plasma") +  # Adjust color palette as needed
+  geom_sf(aes(fill = average_rank)) +
+  scale_fill_viridis_c(option = "plasma", direction=-1) +  # Adjust color palette as needed
   theme_minimal() +
-  labs(title = "Predicted Number of Seniors on SNAP by PUMA",
-       fill = "Predicted number\nof Seniors\non SNAP")
-ggsave("figures/test2.png", width=6, height=5)
+  labs(title = "Finding the average rank of counties with seniors with food insecurity",
+       fill = "Average rank\namong other\ncounties")
+ggsave("figures/average_rank_of_insecurity_seniors.png", width=8, height=5)
